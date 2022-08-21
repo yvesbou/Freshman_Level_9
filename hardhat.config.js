e6ff635a66bf81089369c752e032c3a22b8ce288
yvesbou <63062257+yvesbou@users.noreply.github.com>
2022-08-21 12:56:00 +0200

    finalized level 9

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,32 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+require("@nomiclabs/hardhat-etherscan");
+
+const ALCHEMY_API_KEY_URL = process.env.ALCHEMY_API_KEY_URL;
+const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.9",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.0"
+      },
+      {
+        version: "0.8.1"
+      }
+    ]},
+  networks: {
+    goerli: {
+      chainId: 5,
+      url: ALCHEMY_API_KEY_URL,
+      accounts: [GOERLI_PRIVATE_KEY]
+    },
+  },
+  etherscan: {
+    apiKey: {
+      goerli: ETHERSCAN_API_KEY
+    },
+  },
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,26 +1,18 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
-
-  const lockedAmount = hre.ethers.utils.parseEther("1");
-
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  const nfteeContract = await hre.ethers.getContractFactory("GameItem");
+  const deployedNFTContract = await nfteeContract.deploy();
+  
+  await deployedNFTContract.deployed();
+  
+  console.log("NFT Contract Address:", deployedNFTContract.address);
+  
+  // verify on etherscan
+  await deployedNFTContract.deployTransaction.wait(10);
+  await hre.run("verify:verify", {
+    address: deployedNFTContract.address,
+  });
 }
 
 // We recommend this pattern to be able to use async/await everywhere
